39c20399911b8c41e38bb5629488de2cba2c4c6f
Giovanni Propersi <giovannigp@gmail.com>
2022-01-30 22:25:41 -0600

    Added assets, and a LaunchScreen, and a new ViewController for the app.

---
+++ flixster/flixster/MoviesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Giovanni Propersi on 1/30/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Any additional setup after loading the view
+    }
+}
